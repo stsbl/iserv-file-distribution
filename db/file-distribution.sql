f56657289e45135ce7d84661508294b27b900acb
Felix Jacobi <felix.jacobi@stsbl.de>
2017-02-08 01:28:40 +0100

    Setup packaging.

---
+++ db/file-distribution.sql
@@ -0,0 +1,23 @@
+/**
+ * Author:  Felix Jacobi
+ * Created: 28.11.2016
+ * License: MIT license <https://opensource.org/licenses/MIT> 
+ */
+
+CREATE TABLE file_distribution (
+    ID                  SERIAL          PRIMARY KEY,
+    Title               TEXT            NOT NULL,
+    Act                 TEXT            NOT NULL REFERENCES users(Act)
+                                            ON UPDATE CASCADE
+                                            ON DELETE CASCADE,
+    Hostname            TEXT            NOT NULL REFERENCES hosts(Name)
+                                            ON UPDATE CASCADE
+                                            ON DELETE CASCADE,
+    IP                  INET            NOT NULL REFERENCES hosts(IP)
+                                            ON UPDATE CASCADE
+                                            ON DELETE CASCADE,
+    UNIQUE(IP, Hostname)
+);
+
+GRANT USAGE, SELECT ON "file_distribution_id_seq" TO "symfony";
+GRANT SELECT, INSERT, UPDATE, DELETE ON "file_distribution" TO "symfony";
