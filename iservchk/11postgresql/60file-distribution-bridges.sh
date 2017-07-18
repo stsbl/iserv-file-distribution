15ad8dbae4e5f4dd717b6dc39bbc388461d9bc4b
Felix Jacobi <felix.jacobi@stsbl.de>
2017-07-18 02:16:15 +0200

    Always remove lock database bridge.

--- iservchk/11postgresql/60file-distribution-bridges.sh
+++ iservchk/11postgresql/60file-distribution-bridges.sh
@@ -16,19 +16,9 @@ Test "remove file distribution db bridge for iserv-exam"
 EOT
 fi
 
-# exclude stsbl.test from bridges, because we do not have package management here
-if [ -f "/var/lib/dpkg/info/iserv-lock.list" ] && [ ! "$(hostname)" = "iserv.stsbl.test" ]
-then
-  cat <<EOT
-Test "install file distribution db bridge for iserv-lock"
-  "[[ \$(dpkg-query --showformat='\${Status}\n' --show 'stsbl-iserv-file-distribution-bridge-lock' 2> /dev/null | grep 'install ok installed') ]]" 
-  "/usr/lib/iserv/aptitude_auto install stsbl-iserv-file-distribution-bridge-lock"
-EOT
-else
-  cat <<EOT
+cat <<EOT
 Test "remove file distribution db bridge for iserv-lock"
   "[[ ! \$(dpkg-query --showformat='\${Status}\n' --show 'stsbl-iserv-file-distribution-bridge-lock' 2> /dev/null | grep 'install ok installed') ]]"
   "/usr/lib/iserv/aptitude_auto remove stsbl-iserv-file-distribution-bridge-lock"
 EOT
-fi
 
