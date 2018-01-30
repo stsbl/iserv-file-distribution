95d2d0264906868dd8f7e2b8d7e9b277f76c6c2f
Felix Jacobi <felix.jacobi@iserv.eu>
2018-01-30 19:43:25 +0100

    Remove old database bridge

--- iservchk/11postgresql/60file-distribution-bridges.sh
+++ iservchk/11postgresql/60file-distribution-bridges.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-# exclude iserv.stsbl.test from bridges, because we do not have package management here
-if [ -f "/var/lib/dpkg/info/iserv-exam.list" ] && [ ! "$(hostname)" = "iserv.stsbl.test" ]
-then
-  cat <<EOT
-Test "install file distribution db bridge for iserv-exam"
-  "[[ \$(dpkg-query --showformat='\${Status}\n' --show 'stsbl-iserv-file-distribution-bridge-exam' 2> /dev/null | grep 'install ok installed') ]]"
-  "/usr/lib/iserv/aptitude_auto install stsbl-iserv-file-distribution-bridge-exam"
-EOT
-else
-  cat <<EOT
+cat <<EOT
 Test "remove file distribution db bridge for iserv-exam"
   "[[ ! \$(dpkg-query --showformat='\${Status}\n' --show 'stsbl-iserv-file-distribution-bridge-exam' 2> /dev/null | grep 'install ok installed') ]]" 
   "/usr/lib/iserv/aptitude_auto remove stsbl-iserv-file-distribution-bridge-exam"
 EOT
-fi
 
 cat <<EOT
 Test "remove file distribution db bridge for iserv-lock"
