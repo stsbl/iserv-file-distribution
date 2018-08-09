d1178e5e166e07912168fd5d8bb5e6e9ee8d1104
Felix Jacobi <felix.jacobi@iserv.eu>
2018-08-09 02:44:07 +0200

    Moved to webpack [wip]

---
+++ src/Stsbl/FileDistributionBundle/Resources/webpack.config.js
@@ -0,0 +1,15 @@
+// src/Stsbl/EximStatsBundle/Resources/webpack.config.js
+let merge = require('webpack-merge');
+let path = require('path');
+let baseConfig = require(path.join(process.env.WEBPACK_BASE_PATH, 'webpack.config.base.js'));
+
+let webpackConfig = {
+    entry: {
+        'js/file-distribution': './assets/js/file-distribution.js',
+        'js/file-distribution-autocomplete': './assets/js/file-distribution-autocomplete.js',
+        'js/file-distribution-highlight': './assets/js/file-distribution-highlight.js',
+        'css/file-distribution.css': './assets/less/file-distribution.less',
+    },
+};
+
+module.exports = merge(baseConfig.get(__dirname), webpackConfig);
